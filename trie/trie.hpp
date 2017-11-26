c00c909ce99ab060ddd0a6a2aa51c1c6cbde6c2d
littlebirds <gangzhi2000@gmail.com>
2017-11-26 15:36:47 -0500

    Update trie.hpp

--- trie/trie.hpp
+++ trie/trie.hpp
@@ -10,16 +10,12 @@ private:
         std::map<char, Node> children;  
         // ctor
         Node(): isEndOfWord(false) {}      
-    };    
-
-    Node root; 
-
+    };  
+    Node root;  
 private:
-    void print(const Node& root, std::string& path) const;
-
+    void print(const Node& root, std::string& path) const; 
 public:
     void insert(const std::string& word);
-    bool contains(const std::string& word) const; 
-    void build_trie(const std::vector<std::string>& words) ;
+    bool contains(const std::string& word) const;  
     void display( ) const;
 }; 
