4b2625f591c578fab35ac16dd9c9dff23395cf63
littlebirds <gangzhi2000@gmail.com>
2017-11-26 15:35:08 -0500

    header for simple trie class

---
+++ trie/trie.hpp
@@ -0,0 +1,25 @@
+#include <memory>
+#include <string>
+#include <map>
+#include <vector>
+
+class Trie {
+private:
+    struct Node {
+        bool isEndOfWord;
+        std::map<char, Node> children;  
+        // ctor
+        Node(): isEndOfWord(false) {}      
+    };    
+
+    Node root; 
+
+private:
+    void print(const Node& root, std::string& path) const;
+
+public:
+    void insert(const std::string& word);
+    bool contains(const std::string& word) const; 
+    void build_trie(const std::vector<std::string>& words) ;
+    void display( ) const;
+}; 
